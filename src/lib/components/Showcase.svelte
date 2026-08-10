9ed0bb672ccb06a07bf0c90a96d680354194ed11
mariopurdescu23-cpu <mariopurdescu23@gmail.com>
2026-08-10 15:58:58 +0300

    tes

--- src/lib/components/Showcase.svelte
+++ src/lib/components/Showcase.svelte
@@ -131,7 +131,7 @@
 		<div class="relative w-full">
 			<div
 				bind:this={carouselRef}
-				class="flex lg:grid lg:grid-cols-5 gap-6 overflow-x-auto lg:overflow-visible snap-x snap-mandatory lg:snap-none pb-4 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+				class="flex lg:grid lg:grid-cols-5 gap-6 overflow-x-auto lg:overflow-visible snap-x snap-mandatory lg:snap-none pb-4 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden [touch-action:none] lg:[touch-action:auto]"
 			>
 				{#each services as service}
 					<a
